7eb16d31b05af4fc553c0a1efdfe4bf49919178e
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-07-15 12:43:15 +0900

    init

---
+++ backend/src/main/java/com/ssafy/foss/FossApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.foss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FossApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/foss/FossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.foss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
