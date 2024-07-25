0fbdc0e00ba32b93f7f8dd1caff33277a23e7f68
gyeong_min <nkh9587@naver.com>
2024-07-25 17:14:05 +0900

    🐛 fix : 멘티 일정 관련 수정
    - 멘티 일정 신청
    - 멘티 일정 삭제
    - 신청한 일정 조회

--- backend/src/main/java/com/ssafy/foss/schedule/repository/ApplyRepository.java
+++ backend/src/main/java/com/ssafy/foss/schedule/repository/ApplyRepository.java
@@ -4,8 +4,13 @@ import com.ssafy.foss.schedule.domain.Apply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
     List<Apply> findByScheduleId(Long scheduleId);
+    List<Apply> findByMemberId(Long memberId);
+    Optional<Apply> findByScheduleIdAndMemberId(Long scheduleId, Long memberId);
     Long countByScheduleId(Long scheduleId);
+    void deleteAllByScheduleId(Long scheduleId);
+
 }
