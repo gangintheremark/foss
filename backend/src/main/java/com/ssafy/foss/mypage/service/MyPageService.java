b9c2197ebf5fc0cc984abe8d8ad8fb6aedaa86eb
gyeong_min <nkh9587@naver.com>
2024-07-31 16:47:40 +0900

    ✨ feat : 멘토 정보 및 경력사항 저장 API

---
+++ backend/src/main/java/com/ssafy/foss/mypage/service/MyPageService.java
@@ -0,0 +1,34 @@
+package com.ssafy.foss.mypage.service;
+
+import com.ssafy.foss.career.domain.Career;
+import com.ssafy.foss.career.dto.AddCareerRequest;
+import com.ssafy.foss.career.service.CareerService;
+import com.ssafy.foss.company.domain.Company;
+import com.ssafy.foss.company.service.CompanyService;
+import com.ssafy.foss.member.domain.Member;
+import com.ssafy.foss.member.domain.Role;
+import com.ssafy.foss.member.service.MemberService;
+import com.ssafy.foss.mentorInfo.dto.CreateMentorInfoAndCareerRequest;
+import com.ssafy.foss.mentorInfo.service.MentorInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class MyPageService {
+
+    private final MentorInfoService mentorInfoService;
+    private final MemberService memberService;
+    private final CareerService careerService;
+
+    @Transactional
+    public void createMentorInfoAndCareer(Long memberId, CreateMentorInfoAndCareerRequest createMentorInfoAndCareerRequest, MultipartFile file) {
+        mentorInfoService.createMentorInfo(memberId, createMentorInfoAndCareerRequest.getSelfProduce(), file);
+        careerService.createCareers(memberId, createMentorInfoAndCareerRequest.getAddCareerRequests());
+        Member member = memberService.findById(memberId);
+        member.setRole(Role.MENTOR);
+    }
+}
