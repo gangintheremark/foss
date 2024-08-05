4c06e52455405e809b663f414c49e91c2ccc407a
gyeong_min <nkh9587@naver.com>
2024-08-05 12:41:22 +0900

    :art: style : ts로 변경

--- frontend/src/components/MyPage/ProfileSetting.tsx
+++ frontend/src/components/MyPage/ProfileSetting.tsx
@@ -499,7 +499,7 @@ const ProfileSetting = ({
 
   return (
     <div>
-      <table className="w-full border-collapse mt-5">
+      <table className="w-full border-collapse mt-5 ms-3">
         <tbody>
           <tr>
             <td className="w-32 p-4 font-semibold text-gray-700">이미지</td>
