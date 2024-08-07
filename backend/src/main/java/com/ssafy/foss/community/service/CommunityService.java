bed00355a3a4664463e611e5ec52117ff567c022
FickleBoBo <yukmw0704@gmail.com>
2024-08-07 15:21:10 +0900

    🐛 fix : 백엔드 자유게시판 코드 최신화

--- backend/src/main/java/com/ssafy/foss/community/service/CommunityService.java
+++ backend/src/main/java/com/ssafy/foss/community/service/CommunityService.java
@@ -1,6 +1,7 @@
 package com.ssafy.foss.community.service;
 
 import com.ssafy.foss.community.domain.Post;
+import com.ssafy.foss.community.dto.PostResponse;
 import com.ssafy.foss.community.repository.CommunityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,25 +18,44 @@ public class CommunityService {
     @Autowired
     private CommunityRepository communityRepository;
 
-    // 모든 게시물 조회
-    public List<Post> getAllPosts() {
-        return communityRepository.findAll();
-    }
+//    // 페이지별 게시물 조회
+//    public Page<Post> getPostsByPage(int page, int size) {
+//        // 클라이언트 페이지 번호는 1부터 시작하므로, 서버 측에서는 0부터 시작하는 페이지 번호로 변환
+//        Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, "regDate")); // 날짜 기준 내림차순 정렬
+//        return communityRepository.findAll(pageable);
+//    }
 
-    // 페이지별 게시물 조회
-    public Page<Post> getPostsByPage(int page, int size) {
+    // 키워드로 게시물 검색
+    public Page<Post> searchPostsByKeyword(String keyword, int page, int size) {
         // 클라이언트 페이지 번호는 1부터 시작하므로, 서버 측에서는 0부터 시작하는 페이지 번호로 변환
         Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, "regDate")); // 날짜 기준 내림차순 정렬
-        return communityRepository.findAll(pageable);
+        return communityRepository.findByKeyword(keyword, pageable);
     }
 
     // 특정 게시물 조회
-    public Post getPostById(Long id) {
-        return communityRepository.findById(id).orElse(null);
+    public PostResponse getPostById(Long postId, Long memberId) {
+        Post post = communityRepository.findById(postId).orElse(null);
+        if(post == null) return null;
+
+        boolean isOwner = post.getMemberId().equals(memberId);
+
+        PostResponse postResponse = PostResponse.builder()
+                .postId(post.getPostId())
+                .memberId(post.getMemberId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .writer(post.getWriter())
+                .regDate(post.getRegDate())
+                .isOwner(isOwner)
+                .build();
+
+        return postResponse;
     }
 
     // 게시물 생성
-    public Post createPost(Post post) {
+    public Post createPost(Post post, Long memberId, String writer) {
+        post.setMemberId(memberId);
+        post.setWriter(writer);
         return communityRepository.save(post);
     }
 
