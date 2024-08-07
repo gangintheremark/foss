c8117c45c76c354201d4c848dc428680140360b2
gyeong_min <nkh9587@naver.com>
2024-08-07 17:37:47 +0900

    :art: style : 이미지 크기 수정

--- frontend/src/components/Register/MentorIntro.tsx
+++ frontend/src/components/Register/MentorIntro.tsx
@@ -9,7 +9,7 @@ const MentorIntro = (props: Props) => {
   return (
     <>
       <div className="h-[213px] w-full flex flex-col gap-3 font-notoKR font-black text-[#929292]">
-        <img src={props.profileImg} className="w-full h-full object-cover" />
+        <img src={props.profileImg} className="w-60 h-60 object-cover rounded-3xl" />
         <div className="text-base">{props.name}</div>
         <div className="text-3xl text-black">{props.companyName}</div>
         <div className="mb-4 text-base">{props.department}</div>
