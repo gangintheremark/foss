1c0ffb86f4422b85acfb68a8c3665d22d53e71a7
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-08-07 14:24:34 +0900

    🐛 fix : WebConfig 수정

--- backend/src/main/java/com/ssafy/foss/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/foss/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173", "https://localhost:5173",
+                .allowedOrigins("http://localhost:5173", "https://localhost:5173",
                         "http://i11a705.p.ssafy.io:5173", "https://i11a705.p.ssafy.io:5173",
                         "https://i11a705.p.ssafy.io:8080") // 허용할 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION") // 허용할 HTTP 메소드 설정
