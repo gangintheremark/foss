52e0c80483058fb9ec5dcd0bb93f214f50ba7833
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-08-07 14:16:57 +0900

    🐛 fix : WebConfig 수정
    CORS 허용

--- backend/src/main/java/com/ssafy/foss/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/foss/config/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // 허용할 도메인 설정
+                .allowedOriginPatterns("http://localhost:5173", "https://localhost:5173",
+                        "http://i11a705.p.ssafy.io:5173", "https://i11a705.p.ssafy.io:5173",
+                        "https://i11a705.p.ssafy.io:8080") // 허용할 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*")
                 .allowCredentials(true); // 인증 정보 허용 여부
