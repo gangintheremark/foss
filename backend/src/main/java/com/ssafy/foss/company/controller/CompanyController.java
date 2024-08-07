73d7f3273e0478ef84a3584fbfa61ae45af1759e
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-08-07 10:48:40 +0900

    ✨ feat : 모든 회사 정보 조회

--- backend/src/main/java/com/ssafy/foss/company/controller/CompanyController.java
+++ backend/src/main/java/com/ssafy/foss/company/controller/CompanyController.java
@@ -14,16 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Tag(name = "CompanyController", description = "회사 API")
-@RequestMapping("/companys")
+@RequestMapping("/companies")
 @RestController
 @RequiredArgsConstructor
 public class CompanyController {
     private final CompanyService companyService;
 
-    @Operation(summary = "회사명 검색", description = "키워드로 회사명을 찾을 수 있습니다.")
+//    @Operation(summary = "회사명 검색", description = "키워드로 회사명을 찾을 수 있습니다.")
+//    @GetMapping
+//    public ResponseEntity<List<CompanyResponse>> findAllByWord(@RequestParam String searchWord) {
+//        return ResponseEntity.ok(companyService.findAllBySearchWord(searchWord));
+//    }
+
+    @Operation(summary = "모든 회사 정보 조회", description = "모든 회사의 식별자, 회사명을 조회합니다.")
     @GetMapping
-    public ResponseEntity<List<CompanyResponse>> findAllByWord(@RequestParam String searchWord) {
-        return ResponseEntity.ok(companyService.findAllBySearchWord(searchWord));
+    public ResponseEntity<List<CompanyResponse>> findAll() {
+        return ResponseEntity.ok(companyService.findAll());
     }
 
 }
