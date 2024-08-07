547660bd11194fda41f2e66fb1b13cda31885514
gyeong_min <nkh9587@naver.com>
2024-08-07 09:36:09 +0900

    🐛 fix : 배경 삭제

--- frontend/src/components/Feedback/FeedBackView.tsx
+++ frontend/src/components/Feedback/FeedBackView.tsx
@@ -9,14 +9,13 @@ const FeedBackView = (props: FeedBackReducer) => {
   const { setData } = useFeedBackStore((state) => state.actions);
   return (
     <div className="flex flex-col justify-center items-center">
-      <Bgblur className="absolute bottom-0 left-0" />
       <div className="w-11/12">
         <Intro
-          title="면접 미팅 피드백"
-          sub="닉네임이 참여하신 면접 미팅에서의 피드백을 확인하세요."
+          title="모의 면접 피드백"
+          sub="참여하신 면접에서의 피드백을 확인하세요."
         />
       </div>
-      <div className="w-2/3 min-w-[960px] flex flex-wrap gap-9 my-10">
+      <div className="w-2/3 min-w-[960px] flex flex-wrap gap-9 mb-10">
         {data.map((e, i) => {
           return (
             <FeedbackCard
