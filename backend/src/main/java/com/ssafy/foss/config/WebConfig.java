ee8846fbdace200866c8c891ef66ecefba5bc55f
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-08-08 15:32:54 +0900

    🐛 fix : 도메인 변경

--- backend/src/main/java/com/ssafy/foss/config/WebConfig.java
+++ backend/src/main/java/com/ssafy/foss/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "https://localhost:5173",
                         "http://i11a705.p.ssafy.io:5173", "https://i11a705.p.ssafy.io:5173",
-                        "https://i11a705.p.ssafy.io:8080") // 허용할 도메인 설정
+                        "https://i11a705.p.ssafy.io:8080", "https://i11a705.p.ssafy.io") // 허용할 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드 설정
                 .allowedHeaders(CorsConfiguration.ALL)
                 .allowCredentials(true) // 인증 정보 허용 여부
