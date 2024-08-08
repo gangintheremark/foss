41cfedc32e8e30aad20b16a44f2e8b77748784bb
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-08-08 14:08:50 +0900

    🐛 fix : 잠시 주석

--- backend/src/main/java/com/ssafy/foss/returnzeroAI/controller/ReturnZeroController.java
+++ backend/src/main/java/com/ssafy/foss/returnzeroAI/controller/ReturnZeroController.java
@@ -1,31 +1,31 @@
-package com.ssafy.foss.returnzeroAI.controller;
-
-import com.ssafy.foss.returnzeroAI.responseDTO.AuthResponse;
-import com.ssafy.foss.returnzeroAI.service.ReturnZeroService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@CrossOrigin
-@RestController
-@RequestMapping("/returnzero")
-public class ReturnZeroController {
-
-    @Autowired
-    private ReturnZeroService returnZeroService;
-
-    // 리턴제로야 토큰줘
-    @GetMapping("/authenticate")
-    public ResponseEntity<AuthResponse> authenticate() {
-
-
-        AuthResponse authResponse = returnZeroService.getToken();
-
-        return ResponseEntity.ok(authResponse);
-    }
-
-    // 리턴제로야 데이터 줘
-}
+//package com.ssafy.foss.returnzeroAI.controller;
+//
+//import com.ssafy.foss.returnzeroAI.responseDTO.AuthResponse;
+//import com.ssafy.foss.returnzeroAI.service.ReturnZeroService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.*;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@CrossOrigin
+//@RestController
+//@RequestMapping("/returnzero")
+//public class ReturnZeroController {
+//
+//    @Autowired
+//    private ReturnZeroService returnZeroService;
+//
+//    // 리턴제로야 토큰줘
+//    @GetMapping("/authenticate")
+//    public ResponseEntity<AuthResponse> authenticate() {
+//
+//
+//        AuthResponse authResponse = returnZeroService.getToken();
+//
+//        return ResponseEntity.ok(authResponse);
+//    }
+//
+//    // 리턴제로야 데이터 줘
+//}
--- backend/src/main/java/com/ssafy/foss/returnzeroAI/responseDTO/AuthResponse.java
+++ backend/src/main/java/com/ssafy/foss/returnzeroAI/responseDTO/AuthResponse.java
@@ -1,12 +1,12 @@
-package com.ssafy.foss.returnzeroAI.responseDTO;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
-public class AuthResponse {
-
-    private String token;
-    private int expiresIn;
-}
+//package com.ssafy.foss.returnzeroAI.responseDTO;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Setter
+//@Getter
+//public class AuthResponse {
+//
+//    private String token;
+//    private int expiresIn;
+//}
