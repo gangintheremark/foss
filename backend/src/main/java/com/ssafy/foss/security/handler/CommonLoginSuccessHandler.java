1ca5b54b04011f0fb52f225df68773ac9593a634
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2024-08-09 09:17:44 +0900

    Update CommonLoginSuccessHandler.java

--- backend/src/main/java/com/ssafy/foss/security/handler/CommonLoginSuccessHandler.java
+++ backend/src/main/java/com/ssafy/foss/security/handler/CommonLoginSuccessHandler.java
@@ -26,6 +26,6 @@ public class CommonLoginSuccessHandler implements AuthenticationSuccessHandler {
         String tempToken = JwtUtils.generateToken(responseMap, 5);
 
         log.info("로그인한 사용자 식별자: " + responseMap.get("id"));
-        response.sendRedirect("http://localhost:8080?tempToken=" + tempToken);
+        response.sendRedirect("http://localhost:5173?tempToken=" + tempToken);
     }
 }
