50ca928915acbcb1c18c36f1049b81cac48cfc77
gyeong_min <nkh9587@naver.com>
2024-08-11 14:45:13 +0900

    :bug: fix : 필요없는 import 제거

--- backend/src/main/java/com/ssafy/foss/schedule/component/ScheduleCleanupTask.java
+++ backend/src/main/java/com/ssafy/foss/schedule/component/ScheduleCleanupTask.java
@@ -1,13 +1,9 @@
 package com.ssafy.foss.schedule.component;
 
-import com.ssafy.foss.apply.repository.ApplyRepository;
 import com.ssafy.foss.apply.service.ApplyService;
 import com.ssafy.foss.schedule.domain.Schedule;
-import com.ssafy.foss.schedule.repository.ApplyRepository;
 import com.ssafy.foss.schedule.repository.ScheduleRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
