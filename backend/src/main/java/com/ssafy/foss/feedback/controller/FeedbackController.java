8984757a68e166114716318f4fb715b3646d965a
rkdmf1026 <50603273+rkdmf1026@users.noreply.github.com>
2024-08-12 12:36:35 +0900

    🔨 refactor : API 수정

--- backend/src/main/java/com/ssafy/foss/feedback/controller/FeedbackController.java
+++ backend/src/main/java/com/ssafy/foss/feedback/controller/FeedbackController.java
@@ -79,8 +79,8 @@ public class FeedbackController {
 
     @Operation(summary = "리뷰 작성 유무 조회", description = "리뷰 작성 유무 조회")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK"), @ApiResponse(responseCode = "404", description = "Page Not Found"), @ApiResponse(responseCode = "500", description = "Internal Server Error")})
-    @GetMapping("/checkReview")
-    public ResponseEntity<?> findIsReviewCheck(@PathVariable("respondent_id") Long respondentId, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+    @PostMapping("/checkReview")
+    public ResponseEntity<?> findIsReviewCheck(@RequestBody Long respondentId, @AuthenticationPrincipal PrincipalDetail principalDetail) {
         return ResponseEntity.ok().body(feedbackService.findIsCheckReview(respondentId));
     }
 }
