821c53b54d6b19e8f0d410006c5ae761845cd695
조민규 <ariettys2@naver.com>
2024-08-12 09:08:33 +0900

    🐛 fix : console 확인 코드 추가 및 구성 위치 변경

--- frontend/src/components/Feedback/FeedBackDetailForm.tsx
+++ frontend/src/components/Feedback/FeedBackDetailForm.tsx
@@ -12,6 +12,7 @@ function FeedBackDetailForm({
 }): ReactNode {
   // const { mentorInfo, open } = useFeedBackStore((state) => state.states);
   const { setMenteeId, setOpen } = useFeedBackStore((state) => state.actions);
+  console.log(feedback);
   if (typeof feedback === 'string') {
     return (
       <>
